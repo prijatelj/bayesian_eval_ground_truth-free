163340918794ff72598b5181d8223c0f4f3f9210
mel-2445 <mfmccurrie@cox.net>
2019-03-26 17:22:21 -0400

    add some structure

---
+++ set_environment.sh
@@ -0,0 +1,6 @@
+export ROOT=$PWD
+export PYTHONPATH=$PWD:$PYTHONPATH
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64"
+export LIBRARY_PATH="$LIBRARY_PATH:/usr/local/cuda/lib64"
+export CUDA_HOME=/usr/local/cuda-9.2/
+export PATH=/usr/local/cuda-9.2/bin:$PATH
