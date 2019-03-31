837a5a6450af1e2d72067958cbffbdee91ba9426
mel-2445 <mfmccurrie@cox.net>
2019-03-31 12:33:16 -0400

    update set_env

--- set_environment.sh
+++ set_environment.sh
@@ -6,3 +6,5 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64"
 export LIBRARY_PATH="$LIBRARY_PATH:/usr/local/cuda/lib64"
 export CUDA_HOME=/usr/local/cuda-9.2/
 export PATH=/usr/local/cuda-9.2/bin:$PATH
+
+alias metric="cd $ROOT"
