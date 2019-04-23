db6bf43661b8b73521b346f66a085748ebcc375c
prijatelj <theomnipotentwoof@gmail.com>
2019-04-22 23:04:30 -0400

    WIP: working to standardize, stopping to focus on truth survey from beginning to end.

--- psych_metric/datasets/ipeirotis2010/dataset.py
+++ psych_metric/datasets/ipeirotis2010/dataset.py
@@ -18,19 +18,21 @@ class Ipeirotis2010(BaseDataset):
     ----------
     dataset : str
         Name of specific dataset
-    annotations : pandas.DataFrame
-        Data Frame containing annotations
+    df : pandas.DataFrame
+        Contains the data of the dataset in a standardized format. Typically
+        that format is an annotation list where each row is an individual's
+        annotation of one sample. Must contain the columns: 'worker_id' and
+        'label'.  'ground_truth' is also a common column name when ground truth
+        is included with the original dataset. 'sample_id' will exist when no
+        features are provided, or some features need loaded.
     label_set : set
         Set containing the complete original labels
     label_encoder : {str: sklearn.preprocessing.LabelEncoder}
         Dict of column name to Label Encoder for the labels. None if no
         encodings used.
-    sparse_matrix : bool
-        Dataframe uses datafile structure if True, uses sparse matrix format if
-        False. Default value is False
     """
 
-    def __init__(self, dataset='AdultContent', encode_columns=None, sparse_matrix=False):
+    def __init__(self, dataset='AdultContent', encode_columns=None):
         """initialize class by loading the data
 
         Parameters
@@ -43,6 +45,7 @@ class Ipeirotis2010(BaseDataset):
         sparse_matrix : bool, optional
             Convert the data into a dataframe with the sparse matrix structure
         """
+    def load_dataset(self, dataset='AdultContent', encode_columns=None):
         dsets = [
             'AdultContent', 'AdultContent2', 'AdultContent3',
             'BarzanMozafari', 'CopyrightInfringement',
@@ -50,22 +53,30 @@ class Ipeirotis2010(BaseDataset):
             'JeroenVuurens'
         ]
         self._check_dataset(dataset, dsets)
+
+        # TODO separate these conditionals into data specific load functions.
+
         if dataset == 'AdultContent3':
             self.dataset = 'AdultContent3-HCOMP2010'
         else:
             self.dataset = dataset
 
-        if not isinstance(sparse_matrix, bool):
-            raise TypeError('sparse_matrix parameter must be a boolean.')
-        self.sparse_matrix = sparse_matrix
-
         # Read in and save data
         if dataset == 'JeroenVuurens':
             annotation_file = os.path.join(HERE, 'ipeirotis2010_data', self.dataset, 'votes')
-            self.annotations = pd.read_csv(annotation_file, header=None, names=['time', 'question_id', 'worker_id', 'label'], delimiter='\t')
+            self.df = pd.read_csv(annotation_file, names=['time', 'question_id', 'worker_id', 'label'], delimiter='\t')
         else:
             annotation_file = os.path.join(HERE, 'ipeirotis2010_data', self.dataset, 'labels.txt')
-            self.annotations = pd.read_csv(annotation_file, header=None, names=['worker_id', 'url', 'label'], delimiter='\t')
+            self.df = pd.read_csv(annotation_file, names=['worker_id', 'url', 'label'], delimiter='\t')
+            # reorder columns into format of features, worker_id, label
+            self.df = self.df[['url', 'worker_id', 'label']]
+
+        # Read in ground truth.
+        if 'Adult' in dataset or dataset == 'CopyrightInfringement':
+            ground_truth_file = os.path.join(HERE, 'ipeirotis2010_data', self.dataset, 'gold.txt')
+            sample_id = 'url'
+            ground_truth_dict = self.read_csv_to_dict(ground_truth_file, sep='\t')
+            self.add_ground_truth_to_samples(ground_truth_dict, is_dict=True, sample_id=sample_id)
 
         # Read in and save the expected labels, or infer the labels from data
         if 'HITspam' in dataset:
@@ -76,12 +87,10 @@ class Ipeirotis2010(BaseDataset):
             labels_file = os.path.join(HERE, 'ipeirotis2010_data', self.dataset, 'categories.txt')
             self.label_set = self._load_labels(labels_file)
 
-        # TODO automate the encoding of columns if encode_columns is True:
-        # ie. hardcode columns for each dataset to be encoded if True.
         # Encode the labels and data if desired
         if 'AdultContent' in dataset or dataset == 'CopyrightInfringement' or 'HITspam' in dataset:
             if encode_columns == True:
-                encode_columns = {'worker_id', 'url', 'label'}
+                encode_columns = {'url', 'worker_id', 'label'}
             column_labels = {'label': self.label_set}
         elif dataset == 'BarzanMozafari':
             if encode_columns == True:
@@ -96,9 +105,31 @@ class Ipeirotis2010(BaseDataset):
 
         self.label_encoder =  None if encode_columns is None else self.encode_labels(encode_columns, column_labels)
 
-        # Restructure dataframe into a sparse matrix
-        if sparse_matrix:
-            self.annotations = self.convert_to_sparse_matrix(annotations)
+    def load_adult(dataset, encode_columns=None):
+        # TODO WIP separating all of these for cleaner layout.
+        self._check_dataset(dataset, ['AdultContent', 'AdultContent2', 'AdultContent3'])
+
+        # Load annotation data
+        annotation_file = os.path.join(HERE, 'ipeirotis2010_data', self.dataset, 'labels.txt')
+        self.df = pd.read_csv(annotation_file, names=['worker_id', 'url', 'label'], delimiter='\t')
+        # reorder columns into format of features, worker_id, label
+        self.df = self.df[['url', 'worker_id', 'label']]
+
+        # Load ground truth
+        ground_truth_file = os.path.join(HERE, 'ipeirotis2010_data', self.dataset, 'gold.txt')
+        ground_truth_dict = self.read_csv_to_dict(ground_truth_file, sep='\t')
+        self.add_ground_truth_to_samples(ground_truth_dict, is_dict=True, sample_id='url')
+
+        # Read in and save the expected labels, or infer the labels from data.
+        labels_file = os.path.join(HERE, 'ipeirotis2010_data', self.dataset, 'categories.txt')
+        self.label_set = self._load_labels(labels_file)
+
+        # Encode the labels and data
+        if encode_columns == True:
+            encode_columns = {'url', 'worker_id', 'label'}
+        column_labels = {'label': self.label_set}
+
+        self.label_encoder =  None if encode_columns is None else self.encode_labels(encode_columns, column_labels)
 
     def _load_labels(self, csv_path=None):
         """ Load labels from the published dataset categories file, or infer
@@ -116,7 +147,7 @@ class Ipeirotis2010(BaseDataset):
         """
         if csv_path is None:
             # infer labels from data, will exclude any missing labels
-            return frozenset(self.annotations['label'].unique())
+            return frozenset(self.df['label'].unique())
 
         # read in the labels provided
         label_set = set()
@@ -126,8 +157,7 @@ class Ipeirotis2010(BaseDataset):
                 label_set.add(row[0])
         return frozenset(label_set)
 
-
-    def convert_to_sparse_matrix(self, annotations):
+    def convert_to_sparse_matrix(self, df):
         """Convert provided dataframe into a sparse matrix equivalent.
 
         Converts the given dataframe of expected format equivalent to this
@@ -158,7 +188,7 @@ class Ipeirotis2010(BaseDataset):
             number of annotations(rows) in dataset. If sparse matrix, number of
             samples
         """
-        return len(self.annotations)
+        return len(self.df)
 
     def __getitem__(self, i):
         """ get specific row from dataset
@@ -168,5 +198,5 @@ class Ipeirotis2010(BaseDataset):
         dict:
             {header: value, header: value, ...}
         """
-        row = self.annotations.iloc[i]
+        row = self.df.iloc[i]
         return dict(row)
