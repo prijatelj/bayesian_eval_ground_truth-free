3b70826f84c420523b0bed7d29d7f49b0f747434
prijatelj <theomnipotentwoof@gmail.com>
2019-05-12 21:24:38 -0400

    added functionality for checking if datasets in missing data collections exist and for loading them.

--- psych_metric/datasets/data_handler.py
+++ psych_metric/datasets/data_handler.py
@@ -7,17 +7,23 @@ def dataset_exists(dataset, collection=None)
     """
     if collection is None:
         # checks if the dataset exists in any collection
-        return isinstance(dataset, str) and (dataset in datasets.Snow2017.datasets or dataset in datasets.TruthSurvey2017.datasets or dataset == 'trec-fr10-data')
+        return isinstance(dataset, str) and (dataset in datasets.Snow2017.datasets or dataset in datasets.TruthSurvey2017.datasets or dataset == 'trec-fr10-data', or dataset in datasets.Ipeierotis2010.datasets or dataset in datasets.FacialBeauty.datasets or dataset in datasets.CrowdLayer.datasets)
     elif collection == 'truth_survey_2017':
         # Check if dataset in truth_survey_2017
         return dataset in datasets.TruthSurvey2017.datasets
     elif collection == 'snow_2008':
         # Check if dataset in snow_2008
         return dataset in datasets.Snow2017.datasets
+    elif collection == 'ipierotis_2010':
+        return dataset in datasets.Ipierotis2010.datasets
+    elif collection == 'facial_beauty':
+        return dataset in datasets.FacialBeauty.datasets
+    elif collection == 'crowd_layer':
+        return dataset in datasets.CrowdLayer.datasets
 
     return dataset == 'trec-rf10-data'
 
-def load_dataset(dataset, encode_columns=False):
+def load_dataset(dataset, dataset_filepath=None, encode_columns=False):
     """
     Handler function for calling the correct datahandler class for a given
     dataset.
@@ -34,12 +40,28 @@ def load_dataset(dataset, encode_columns=False):
     """
     #Snow 2018
     if DataHandler.exists(dataset, 'snow_2008'):
-        return datasets.Snow2018(dataset)
+        return datasets.Snow2018(dataset, dataset_filepath)
 
     # Truth Survey 2017
     elif DataHandler.exists(dataset, 'truth_survey_2017'):
-        return datasets.TruthSurvey2017(dataset, encode_columns)
+        return datasets.TruthSurvey2017(dataset, dataset_filepath, encode_columns)
 
     # TREC 2010
     elif DataHandler.exists(dataset, 'trec-fr10'):
-        return datasets.TRECRelevancy2010(dataset, encode_columns)
+        return datasets.TRECRelevancy2010(dataset_filepath, encode_columns)
+
+    # TODO Ipierotis
+    elif DataHandler.exists(dataset, 'ipierotis_2010'):
+        return datasets.Ipierotis2010(dataset, dataset_filepath, encode_columns)
+
+    # TODO Facial Beauty
+    elif DataHandler.exists(dataset, 'facial_beauty'):
+        return datasets.FacialBeauty(dataset, dataset_filepath, encode_columns)
+
+    # TODO First Impressions
+
+    # TODO CrowdLayer
+    elif DataHandler.exists(dataset, 'crowd_layer'):
+        return datasets.CrowdLayer(dataset, dataset_filepath, encode_columns)
+
+    # TODO Google fact evaluation, low priority.
