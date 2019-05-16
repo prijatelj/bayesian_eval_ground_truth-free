322405cde213c2906bf2fef8dc35628425b6fb15
prijatelj <theomnipotentwoof@gmail.com>
2019-05-16 15:43:46 -0400

    added TRECRelevancy2010 to psych_metric.datasets namespace.

--- psych_metric/datasets/__init__.py
+++ psych_metric/datasets/__init__.py
@@ -3,3 +3,4 @@ from .truth_inference_survey_2017.dataset import TruthSurvey2017
 from .ipeirotis_2010.dataset import Ipeirotis2010
 from .crowd_layer.dataset import CrowdLayer
 from .facial_beauty.dataset import FacialBeauty
+from .trec_relevancy_2010.dataset import TRECRelevancy2010
