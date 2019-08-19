c11ef77107f1c855e65c281f04086c975e858e8f
prijatelj <theomnipotentwoof@gmail.com>
2019-08-19 19:46:28 -0400

    WIP: refactoring to include tf loading and default loading along with preprocessing.

--- psych_metric/datasets/dogs/dataset.py
+++ psych_metric/datasets/dogs/dataset.py
@@ -27,7 +27,7 @@ class Dogs(BaseDataset):
         Dict of column name to Label Encoder for the labels. None if no
         encodings used.
     """
-    def __init__(self, dataset=None, dataset_filepath=None, encode_columns=None, ground_truth=False, tf_load=False):
+    def __init__(self, dataset=None, dataset_filepath=None, encode_columns=None, ground_truth=False, tf_load=False, preprocess=False):
         """initialize class by loading the data
 
         Parameters
@@ -43,44 +43,50 @@ class Dogs(BaseDataset):
 
         if dataset_filepath is None:
             if HERE is None or 'images' not in HERE:
-                raise ValueError('A path to the dataset file was not provided either by the `dataset_filepath` parameter or by the ROOT environment variable. Global variable HERE is `%s`. It is recommended to use the `dataset_filepath` parameter to provide the filepath.' % HERE)
+                raise ValueError(f"""A path to the dataset file was not provided
+                either by the `dataset_filepath` parameter or by the ROOT
+                environment variable. Global variable HERE is `{HERE}`. It is
+                recommended to use the `dataset_filepath` parameter to provide
+                the filepath.""")
             dataset_filepath = HERE
         self.data_dir = dataset_filepath
 
         self.task_type = 'classification'
 
+        if tf_load:
+
+        else:
+
+    def load_file(dataset, dataset_filepath, ground_truth=False):
         # Read in and save data
-        annotation_file = os.path.join(dataset_filepath, self.dataset, 'answer.csv')
+        annotation_file = os.path.join(dataset_filepath, dataset, 'answer.csv')
         self.df = pd.read_csv(annotation_file)
-        # change to standardized column names. #sample_id is a feature.
         self.df.columns = ['sample_id', 'worker_id', 'worker_label']
 
         labels_file = os.path.join(dataset_filepath, self.dataset, 'truth.csv')
         ground_truth_df = pd.read_csv(labels_file)
-        # change to standardized column names.
         ground_truth_df.columns = ['sample_id', 'worker_label']
 
-        # Save labels set
-        self.label_set = set(ground_truth_df['worker_label'].unique()) if dataset != 'f201_Emotion_FULL' else None
+        self.label_set = set(ground_truth_df['worker_label'].unique())
 
         # Add ground_truth to the main dataframe as its own column
         if ground_truth:
             self.add_ground_truth(ground_truth_df, inplace=True)
 
-        if encode_columns == True:
-            # The default columns to encode for each data subset.
-            if dataset in {'d_jn-product', 's4_Relevance', 's5_AdultContent'}:
-                encode_columns = {'sample_id', 'worker_id'}
-            elif dataset in {'d_sentiment', 's4_Face Sentiment Identification', 'f201_Emotion_FULL'}:
-                encode_columns = {'worker_id'}
+        # TODO load images
 
-        # Load and save the features if any.
-        self.features = None
 
         # Encode the labels and data if desired.
         self.label_encoder = None if encode_columns is None else self.encode_labels(encode_columns)
 
-    def
+    def preprocess(images, bounding_box_file, output=None):
+        """Preprocess by the bounding box files."""
+
+
+        if output:
+
+
+    def load_tfrecords():
 
     def __len__(self):
         """ get size of dataset
