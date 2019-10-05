c606f0296c9c01c3858dc0d8321440a42c7f46a2
prijatelj <theomnipotentwoof@gmail.com>
2019-10-04 22:57:35 -0500

    WIP: completed basic structure of the testing of different hypothesis distributions by cycling through the given distrib_ids finding the MLE for each.

--- run_distribution_test.py
+++ run_distribution_test.py
@@ -15,15 +15,92 @@ from psych_metric import distribution_tests
 from predictors import load_prep_data
 
 
-if __name__ == '__main__':
-    args = experiment.ioparse_args()
+def test_human_distribs(
+    dataset_id,
+    data_config,
+    distrib_config,
+    mle_args=None,
+    info_criterions=['bic'],
+    random_seed=None,
+):
+    """Hypothesis testing of distributions of the human annotations.
+
+    Parameters
+    ----------
+    dataset_id : str
+        Identifier of dataset.
+    data_config : dict
+        Dict containing information pertaining to the data.
+    distrib_config : dict
+        Dict of str distrib ids to their initial parameters
+    random_seed : int, optional
+        Integer to use as the random seeds for MLE.
+    """
 
     # TODO First, handle distrib test of src annotations
     # Load the src data, reformat as was done in training in `predictors.py`
+    images, labels, bin_classes = load_prep_data(
+        dataset_id,
+        data_config,
+        label_src,
+        model_config['parts'],
+    )
 
     # Find MLE of every hypothesis distribution
+    distrib_mle : {}
 
-    # calculate the different information criterions (Bayes Factor approxed by BIC)
+    for i, (distrib_id, init_params) in enumerate(distrib_config.items()):
+        logging.info(
+            'Hypothesis Distribution %d/%d : %s',
+            i,
+            len(distrib_config),
+            distrib_id,
+        )
 
+        distrib_mle[distrib_id] = distribution_tests.mle_adam(
+            distrib_id,
+            labels,
+            init_params,
+            **mle_args,
+        )
+
+        # calculate the different information criterions (Bayes Factor approxed by BIC)
+        # TODO Loop through top_likelihoods, save BIC
+        if isinstance(distrib_mle[distrib_id], list):
+            for mle_list for distrib_mle[distrib_id]:
+                mle_list.append(calc_info_criterion(
+                    mle_list[0],
+                    mle_list[1],
+                    info_criterions,
+                    len(labels)
+                ))
+        else:
+            distrib_mle.append(calc_info_criterion(
+                    distrib_mle[0],
+                    distrib_mle[1],
+                    info_criterions,
+                    len(labels)
+            ))
+
+    return distrib_mle
+
+
+def calc_info_criterion(mle, params, criterions, num_samples=None):
+    """Calculate information criterions with mle_list and other information."""
+    info_criterion = {}
+
+    if 'bic' in criterions:
+        info_criterion['bic'] = distribution_tests.bic(mle, len(params), num_samples)
+
+    if 'aic' in criterions:
+        info_criterion['aic'] = distribution_tests.aic(mle, len(params))
+
+    if 'hqc' in criterions:
+        info_criterion['hqc'] = distribution_tests.hqc(mle, len(params), num_samples)
+
+    return info_criterion
+
+if __name__ == '__main__':
+    args, data_config, model_config, kfold_cv_args, random_seeds = experiment.io.parse_args()
     # TODO 2nd repeat for focus fold of k folds: load model of that split
     # split data based on specified random_seed
