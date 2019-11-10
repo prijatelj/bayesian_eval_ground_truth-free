8c46a9dfc5d445d52f6db5e89aa8ec3a4ae29869
prijatelj <theomnipotentwoof@gmail.com>
2019-11-10 17:15:53 -0500

    WIP: cleaned up the MVST class, made it so it could use Nedler-Mead to find just the `df` as long as `df` > 2. Will use this in place of the missing MVST in tfp that allows for parameterization with Sigma matrix instead of scale matrix.

--- psych_metric/mvst.py
+++ psych_metric/mvst.py
@@ -1,3 +1,4 @@
+"""Multivariate Student T distribution."""
 import numpy as np
 import scipy
 
@@ -17,47 +18,57 @@ class MultivariateStudentT(object):
         NOT the scale matrix! This is equal to scale @ scale.T
     """
 
-    def __init__(self, df, loc, scale):
+    def __init__(self, df, loc, sigma, scale=False):
         if not isinstance(df, int):
             raise TypeError(f'`df` must be of type integer, not{type(df)}')
         if not isinstance(loc, np.ndarray):
             raise TypeError(f'`loc` must be of type np.ndarray, not {type(loc)}')
         if loc.dtype != np.float:
             raise TypeError(f'`loc` elements must be of type float, not {loc.dtype}')
-        if not isinstance(scale, np.ndarray):
-            raise TypeError(f'`loc` must be of type np.ndarray, not {type(df)}')
-        if scale.dtype != np.float:
-            raise TypeError(f'`scale` elements must be of type float, not {scale.dtype}')
+        if not isinstance(sigma, np.ndarray):
+            raise TypeError(f'`sigma` must be of type np.ndarray, not {type(df)}')
+        if sigma.dtype != np.float:
+            raise TypeError(f'`sigma` elements must be of type float, not {sigma.dtype}')
         # TODO check if scale is a valid positive definite matrix
 
         self.df = df
         self.loc = loc
-        self.scale = scale
+        self.sigma = sigma @ sigma.T if scale else sigma
 
     def log_prob(self, x):
-        return self.log_probability(x, self.df, self.loc, self.scale)
+        return self.log_probability(x, self.df, self.loc, self.sigma)
 
-    def log_probability(self, x, df, loc, scale):
+    def log_probability(self, x, df, loc, sigma):
         dims = len(loc)
 
         # TODO Ensure this is broadcastable
         return (
             scipy.special.gammaln((df + dims) / 2)
             - (df + dims) / 2 * (
-                1 + (1 / df) * (x - loc) * np.linalg.inv(scale) * (x - loc)
+                1 + (1 / df) * (x - loc) * np.linalg.inv(sigma) * (x - loc)
             ) - (
                 scipy.special.gammaln(df / 2)
                 + .5 * (dims * (np.log(df) + np.log(np.pi))
-                    + np.log(np.linalg.norm(scale))
+                    + np.log(np.linalg.norm(sigma))
                 )
             )
         )
+
+    def sample(self, number_samples):
+        """Sample from the distribution."""
+        raise NotImplementedError(
+            'Need to implement sampling from Multivariate Student T distrib.'
+        )
+
+        # TODO sampling, perhaps via MCMC...?
+        return samples
 
     def mvst_neg_log_prob(
         self,
         x,
         data,
         estimate_loc=False,
+        estimate_scale=False,
         constraint_multiplier=1e5,
     ):
         """the Log probability density function of multivariate
@@ -67,37 +78,49 @@ class MultivariateStudentT(object):
         dims = data.shape[0]
 
         # expand the params into their proper forms
-        df = x[0]
+        if isinstance(x, np.ndarray):
+            df = x[0]
+        else:
+            df = x
+
         if estimate_loc:
             # Estimating loc means it is part of x and needs restructured
             loc = x[1 : dims + 1]
-            scale = np.reshape(x[dims + 1:], [dims, dims])
+
+            if estimate_scale:
+                scale = np.reshape(x[dims + 1:], [dims, dims])
+                sigma = scale @ scale.T
+            else:
+                sigma = np.cov(data) * (df - 2) / df
         else:
             # Derive loc as a constant from the data
             loc = data.mean(0)
-            scale = np.reshape(x[1:], [dims, dims])
+            if estimate_scale:
+                scale = np.reshape(x[1:], [dims, dims])
+                sigma = scale @ scale.T
+            else:
+                sigma = np.cov(data) * (df - 2) / df
 
-        # Make scale symmetric, at least.
-        scale = (scale + scale.T) / 2
-
-        loss = -self.log_probability(data, df, loc, scale).sum()
+        # Get the negative log probability of the data
+        loss = - self.log_probability(data, df, loc, sigma).sum()
 
         # apply constraints to variables
         if df <= 2:
             loss += (-df + 2 + 1e-3) * constraint_multiplier
 
-        # Check if scale is a valid positive definite matrix
-        try:
-            np.linalg.cholesky(scale)
-        except:
-            # TODO How to add a constraint to the positive definite matrix `scale`?
-            # could apply the same constraint to the different diagonal values.
+        if estimate_scale:
+            # Check if scale is a valid positive definite matrix
+            try:
+                np.linalg.cholesky(scale)
+            except:
+                # TODO How to add a constraint to the positive definite matrix `scale`?
+                # could apply the same constraint to the different diagonal values.
 
-            # it is a boolean state afaik, so if not positive definite, then return
-            # high value for loss
+                # it is a boolean state afaik, so if not positive definite, then return
+                # high value for loss
 
-            # TODO ensure the use of absolute value is fine.
-            loss += loss**2 * constraint_multiplier
+                # TODO ensure the use of absolute value is fine.
+                loss += loss**2 * constraint_multiplier
 
         return loss
 
@@ -106,7 +129,7 @@ class MultivariateStudentT(object):
         data,
         df=3,
         loc=None,
-        scale=None,
+        sigma=None,
         const=None,
         max_iter=20000,
         nelder_mead_args=None,
@@ -124,18 +147,27 @@ class MultivariateStudentT(object):
 
         if loc is None:
             loc = data.mean(0)
-        if scale is None:
-            # NOTE, this is actually Sigma...
-            scale = np.cov(data) * (df - 2) / df
+            estimate_loc = False
+        else:
+            estimate_loc = True
 
-        init_data = np.concatenate([[df], loc, scale.flatten()])
+        if sigma is None:
+            sigma = np.cov(data) * (df - 2) / df
+            estimate_scale = True
+        else:
+            estimate_scale = True
+
+        if loc is None and sigma is None:
+            init_data = df
+        else:
+            init_data = np.concatenate([[df], loc, sigma.flatten()])
 
         opt_result = scipy.optimize.minimize(
             lambda x: self.mvst_neg_log_prob(x, data),
-            # TODO init_data,
+            init_data,
             args=[data],
             method='Nelder-Mead',
-            maxiter=max_iter,
+            options={'maxiter': max_iter},
         )
 
         assert(opt_result.success)
@@ -143,6 +175,9 @@ class MultivariateStudentT(object):
         opt_x = opt_result.x
 
         # unpackage the parameters
+        if not (estimate_loc or estimate_scale):
+            return opt_x[0]
+
         df = opt_x[0]
         loc = opt_x[1 : data.shape[1] + 1]
         scale = np.reshape(
