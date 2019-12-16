ff708137c4bac0cb992ca768d3c9d0f2739408c2
prijatelj <theomnipotentwoof@gmail.com>
2019-12-16 17:38:46 -0500

    organized and made the simple step size adaptation hmc scripts.

--- experiment/crc_scripts/bnn_mcmc/sssa_hmc/hmc_2e6sssa_lag5_burnin1e7_1e4s.sh
+++ experiment/crc_scripts/bnn_mcmc/sssa_hmc/hmc_2e6sssa_lag5_burnin1e7_1e4s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#$ -pe smp 16
-#$ -N hmc_sssa
+#$ -pe smp 16        # Specify parallel environment and legal core size
+#$ -N 4_hmc_sssa       # Specify job name
 #$ -q gpu
 #$ -l gpu_card=1
 #$ -o logs/mcmc/hmc/logs/
@@ -13,14 +13,12 @@ BASE_PATH="$HOME/Public/psych_metric"
 module add conda
 source activate "$HOME/TF-1.15"
 
-python3 proto_bnn_mcmc.py \
-    "$BASE_PATH/MCMC/HMC/hmc_1e4s_10u_1e4a_1e7burn_5lag_1e6nr_5e-4ss_5nlfs_2e6sssa/" \
+python3 proto_bnn_mcmc.py "$BASE_PATH/MCMC/HMC/SSSA/hmc_2e6sssa_10u_1e7burn_5lag_1e6nr_5e-4ss_5nlfs_1e4s/" \
     --num_hidden 10 \
     --num_samples 10000 \
-    #--adam_epochs 10000 \
     --cpu 1 \
     --cpu_cores 16 \
-    --gpu 1 \
+    --gpu 0 \
     --kernel_id 'HamiltonianMonteCarlo' \
     --num_results 1000000 \
     --step_size 0.0005 \
