b8e976b54ad6c31935214828c67bf0ee499b0b89
prijatelj <theomnipotentwoof@gmail.com>
2019-12-17 22:37:49 -0500

    WIP: changed to len() from shape to be more general.

--- proto_bnn_mcmc.py
+++ proto_bnn_mcmc.py
@@ -165,7 +165,7 @@ def run_nuts(
         # TODO setup NUTS to use adaptative step sizes
         trans_kernel = tfp.mcmc.TransformedTransitionKernel(
             inner_kernel=kernel,
-            bijector=[tfp.bijectors.Identity()] * init_state.shape[0],
+            bijector=[tfp.bijectors.Identity()] * len(init_state.shape),
         )
 
         if step_adjust_id == 'Simple':
