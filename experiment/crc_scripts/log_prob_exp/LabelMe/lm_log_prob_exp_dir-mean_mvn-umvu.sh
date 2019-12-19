e19b6f9de2020e5cc46625295d0c5a869d5bb77d
prijatelj <theomnipotentwoof@gmail.com>
2019-12-18 22:21:43 -0500

    added crc scripts for baselines of log prob experiment.

---
+++ experiment/crc_scripts/log_prob_exp/LabelMe/lm_log_prob_exp_dir-mean_mvn-umvu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#$ -pe smp 16
+#$ -N lm_lpe_dir_mean
+#$ -q long
+#$ -o logs/fb/fq/lpe/logs/
+#$ -e logs/fb/fq/lpe/logs/
+
+BASE_PATH="$HOME/Public/psych_metric"
+
+# set up the environment
+module add conda
+conda activate tf-1.15
+
+# get the random seed for the specific job from file.
+#SEED="$(sed "$SGE_TASK_ID q;d" $BASE_PATH/experiment/random_seeds/random_seeds_count-100.txt)"
+# TODO perhaps perform the experiment on all seeds as separate jobs (job array)
+
+python3 sjd_log_prob_exp.py \
+    "$BASE_PATH/psych_metric/datasets/crowd_layer/crowd_layer_data/" \
+    --dataset_id 'LabelMe' \
+    --label_src 'frequency' \
+    --output_dir "$BASE_PATH/results/sjd/1114_exp_test/LabelMe/sjd_fq_dir-mean_mvn-umvue.json" \
+    --weights_file 'vgg16.h5' \
+    --dir_path "$BASE_PATH/results/predictors/early_stop/fq/LabelMe/vgg16/1114023021/2019-11-08_20-09-30/5_fold_cv/" \
+    --cpu_cores 16 \
+    --log_file "$BASE_PATH/logs/log_prob_exp/dir-mean_mvn-umvue.log" \
+    --log_level 'INFO' \
+    --src_candidates 'dir-mean_mvn-umvu'
