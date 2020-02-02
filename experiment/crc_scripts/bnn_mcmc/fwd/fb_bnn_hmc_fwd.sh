4da6d47e223e290c6805c378071dabffdd0e0c75
prijatelj <theomnipotentwoof@gmail.com>
2020-02-02 18:57:50 -0500

    unable to run with setting the local directory, so using base path.

--- experiment/crc_scripts/bnn_mcmc/fwd/fb_bnn_hmc_fwd.sh
+++ experiment/crc_scripts/bnn_mcmc/fwd/fb_bnn_hmc_fwd.sh
@@ -7,7 +7,6 @@
 #$ -e logs/mcmc/fwd/fb/fq/1114/logs/
 
 BASE_PATH="$HOME/Public/psych_metric"
-cd $BASE_PATH
 
 # set up the environment
 module add conda
@@ -17,12 +16,12 @@ conda activate tf-1.15
 #SEED="$(sed "$SGE_TASK_ID q;d" $BASE_PATH/experiment/random_seeds/random_seeds_count-100.txt)"
 # TODO perhaps perform the experiment on all seeds as separate jobs (job array)
 
-python3 proto_bnn_mcmc.py \
-    "results/sjd/1114_exp_test/FacialBeauty/fq_train_set_human_pred_simplex_labels.json" \
-    --bnn_weights_file "results/sjd/1114_exp_test/FacialBeauty/fq/bnn_samples_2e2/" \
-    --output_dir "results/sjd/1114_exp_test/FacialBeauty/fq/fwd/hmc_bnn_log_prob.json" \
+python3 "$BASE_PATH/experiment/research/bnn/bnn_mcmc_fwd.py" \
+    "$BASE_PATH/results/sjd/1114_exp_test/FacialBeauty/fq_train_set_human_pred_simplex_labels.json" \
+    --bnn_weights_file "$BASE_PATH/results/sjd/1114_exp_test/FacialBeauty/fq/bnn_samples_2e2/" \
+    --output_dir "$BASE_PATH/results/sjd/1114_exp_test/FacialBeauty/fq/fwd/hmc_bnn_log_prob.json" \
     --num_layers 1 \
     --cpu_cores 24 \
     --gpu 0 \
-    --log_file "logs/mcmc/fwd/fb/fq/1114/hmc_bnn_log_prob.log" \
+    --log_file "$BASE_PATH/logs/mcmc/fwd/fb/fq/1114/hmc_bnn_log_prob.log" \
     --log_level 'INFO' \
