fe43c0f6ae4f9b9eae892997b9d36629f404e30a
prijatelj <theomnipotentwoof@gmail.com>
2020-02-02 23:10:06 -0500

    added tmp commenting out of other datasets as they are not in use with main project right now and psych metric packaging needs them to not exist right now.

--- psych_metric/datasets/__init__.py
+++ psych_metric/datasets/__init__.py
@@ -1,9 +1,9 @@
-from .snow_2008.dataset import Snow2008
-from .truth_inference_survey_2017.dataset import TruthSurvey2017
-from .ipeirotis_2010.dataset import Ipeirotis2010
+#from .snow_2008.dataset import Snow2008
+#from .truth_inference_survey_2017.dataset import TruthSurvey2017
+#from .ipeirotis_2010.dataset import Ipeirotis2010
 from .crowd_layer.dataset import CrowdLayer
 from .facial_beauty.dataset import FacialBeauty
-from .trec_relevancy_2010.dataset import TRECRelevancy2010
+#from .trec_relevancy_2010.dataset import TRECRelevancy2010
 # from .aflw.dataset import FirstImpressions
 
 from psych_metric.datasets import data_handler, base_dataset
