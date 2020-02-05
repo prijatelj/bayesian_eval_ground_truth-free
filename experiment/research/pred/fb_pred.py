dd3a4558fe9aa088ce8a626f709e618a959c8dbc
prijatelj <theomnipotentwoof@gmail.com>
2020-02-04 22:31:12 -0500

    WIP: added beginnings of simple load model and get preds from data to save predictions to file for efficient computation (avoid fwd pass of model and having to load it during KNNDE or any experiment).

---
+++ experiment/research/pred/fb_pred.py
@@ -0,0 +1,64 @@
+"""Obtain predictions from a FacialBeauty ResNeXt50 model."""
+import os
+
+from experiment import io
+from experiment.research.sjd import sjd_log_prob_exp
+
+
+def load_fold_save_pred():
+    # load the data and model
+    model, features, labels = sjd_log_prob_exp.load_eval_fold(
+        ,
+    )
+
+    # Get predictions and save to file
+    io.save_json(
+        os.path.join(dir_path, pred_name),
+        {
+            'train': model.predict(features[0], labels[0])
+            'test': model.predict(features[1], labels[1])
+        }
+    )
+
+
+def load_kfolds_save_preds():
+    # TODO given dir containing the different folds' results directories, save
+
+
+def add_custom_args(parser):
+    # TODO add the str id for the target to compare to: 'frequency', 'ground_truth'
+    # Can add other annotator aggregation methods as necessary, ie. D&S.
+    sjd_log_prob_exp.add_human_sjd_args(parser)
+
+    parser.add_argument(
+        '--whole_kfold_exp',
+        action='store_true',
+        help=' '.join([
+            'If given, then expects the directory to contain all the kfold',
+            'results directories to each have their predictions saved.',
+        ]),
+    )
+
+
+if __name__ == '__main__':
+    args = io.parse_args(['mle', 'sjd'], add_custom_args)
+
+    # NOTE be aware that the defaults of SJD args will overwrite src candidates
+    del args.sjd.target_distrib
+    del args.sjd.transform_distrib
+    del args.sjd.independent
+    del args.sjd.mle_args
+
+    logging.info('Loading data for efficiency')
+    # Load data once: features, labels, label_bin
+    data = predictors.load_prep_data(
+        args.dataset_id,
+        vars(args.data),
+        args.label_src,
+        args.model.parts,
+    )
+
+    if args.whole_kfold_exp:
+        load_kfolds_save_preds()
+    else:
+        load_fold_save_pred()
