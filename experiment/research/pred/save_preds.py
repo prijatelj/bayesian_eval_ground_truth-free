1d9e15be74e449a0d64c4458421dc411be426783
prijatelj <theomnipotentwoof@gmail.com>
2020-02-07 17:37:13 -0500

    finished single fold loading and saving of predictions to json.

--- experiment/research/pred/save_preds.py
+++ experiment/research/pred/save_preds.py
@@ -1,28 +1,49 @@
 """Obtain predictions from a FacialBeauty ResNeXt50 model."""
+import logging
 import os
 
 from experiment import io
+from experiment.research import predictors
 from experiment.research.sjd import sjd_log_prob_exp
 
 
-def load_fold_save_pred():
+def load_fold_save_pred(
+    dir_path,
+    weights_file,
+    label_src,
+    summary_name='summary.json',
+    pred_name='pred.json',
+    data=None,
+    save_labels=False,
+):
     # load the data and model
     model, features, labels = sjd_log_prob_exp.load_eval_fold(
-        ,
+        dir_path,
+        weights_file,
+        label_src,
+        summary_name=summary_name,
+        data=data,
+        load_model=True,
+        pred_name=pred_name,
     )
 
     # Get predictions and save to file
-    io.save_json(
-        os.path.join(dir_path, pred_name),
-        {
-            'train': model.predict(features[0], labels[0])
-            'test': model.predict(features[1], labels[1])
-        }
-    )
+    file_to_save = {
+        'train': model.predict(features[0]),
+        'test': model.predict(features[1]),
+    }
 
+    if save_labels:
+        file_to_save['labels'] = {
+            'train': labels[0],
+            'test': labels[1],
+        }
+
+    io.save_json(os.path.join(dir_path, pred_name), file_to_save)
 
 def load_kfolds_save_preds():
     # TODO given dir containing the different folds' results directories, save
+    return
 
 
 def add_custom_args(parser):
@@ -37,6 +58,12 @@ def add_custom_args(parser):
             'If given, then expects the directory to contain all the kfold',
             'results directories to each have their predictions saved.',
         ]),
+    )
+
+    parser.add_argument(
+        '--save_labels',
+        action='store_true',
+        help='Save the labels in the JSON of the predictions.'
     )
 
 
@@ -61,4 +88,11 @@ if __name__ == '__main__':
     if args.whole_kfold_exp:
         load_kfolds_save_preds()
     else:
-        load_fold_save_pred()
+        load_fold_save_pred(
+            dir_path=args.human_sjd.dir_path,
+            weights_file=args.human_sjd.model_weights_file,
+            label_src=args.label_src,
+            summary_name=args.human_sjd.summary_name,
+            data=data,
+            save_labels=args.save_labels,
+        )
