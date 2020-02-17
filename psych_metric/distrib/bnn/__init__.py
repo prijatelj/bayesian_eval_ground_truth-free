4c98313be2531817f8def1794b1cee317353c4b5
prijatelj <theomnipotentwoof@gmail.com>
2020-02-17 18:28:10 -0500

    corrected import

--- psych_metric/distrib/bnn/__init__.py
+++ psych_metric/distrib/bnn/__init__.py
@@ -1,1 +1,5 @@
 from psych_metric.distrib.bnn import bnn_mcmc
+from psych_metric.distrib.bnn import mcmc
+# TODO need to fix the namespace such that only what I want from each file is
+# pulled into the package, not tensorflow as tf being accessible from these
+# files.
