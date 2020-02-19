076a16dbfc16c49fc1cc8a87bd196bdbebb60c6a
prijatelj <theomnipotentwoof@gmail.com>
2020-02-18 23:07:46 -0500

    added missing back slash to continue the command call to script.

--- experiment/crc_scripts/bnn_mcmc/softmax/hmc/sssa/debug/hmc_sampling_5u_lag10_1e3s.sh
+++ experiment/crc_scripts/bnn_mcmc/softmax/hmc/sssa/debug/hmc_sampling_5u_lag10_1e3s.sh
@@ -15,7 +15,7 @@ conda activate psych_exp_tf-1.15
 
 python3 $BASE_PATH/experiment/research/bnn/proto_bnn_mcmc.py \
     "/afs/crc.nd.edu/group/cvrl/scratch_21/dprijate/results/debug/bnn_softmax/sim/samples_1000/sssa/initial_run_gen_data/data_for_bnn.json" \
-    --output_dir "/afs/crc.nd.edu/group/cvrl/scratch_21/dprijate/results/debug/bnn_softmax/sim/samples_1000/sampling/hmc_5u_8e-4ss_39670lag_1e3res/s2e4/job_$SGE_TASK_ID/" 
+    --output_dir "/afs/crc.nd.edu/group/cvrl/scratch_21/dprijate/results/debug/bnn_softmax/sim/samples_1000/sampling/hmc_5u_8e-4ss_39670lag_1e3res/s2e4/job_$SGE_TASK_ID/" \
     --bnn_weights_file "/afs/crc.nd.edu/group/cvrl/scratch_21/dprijate/results/debug/bnn_softmax/sim/samples_1000/converging/hmc_5u_8e-4ss_1e5res/last_weights.json" \
     --num_hidden 5 \
     --cpu 1 \
