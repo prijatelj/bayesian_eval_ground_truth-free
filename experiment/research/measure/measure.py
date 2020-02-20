b67747c0af2e30ba48885d079bb1044e881e45c4
prijatelj <theomnipotentwoof@gmail.com>
2020-02-20 16:56:04 -0500

    added euclidean distance function, save raw func, and dicitonary creation of summary of measurements.

--- experiment/research/measure/measure.py
+++ experiment/research/measure/measure.py
@@ -24,6 +24,46 @@ def summary_arr(arr, quantile_set=None, axis=None):
 
     return summary
 
+
+def summary_dict(measurements, quantile_set=None, axis=1):
+    """Returns the summaries of different statistics of the measurements. By default, expects the measurements to be of shape
+
+        [samples, num_measurements_per_sample]
+    """
+    return {
+        'overview': summary_arr(measurements, quantile_set=quantile_set),
+        'summary_of_means': summary_arr(
+            np.mean(measurements, axis=axis),
+            quantile_set,
+        ),
+        'summary_of_maxs': summary_arr(
+            np.max(measurements, axis=axis),
+            quantile_set,
+        ),
+        'summary_of_mins': summary_arr(
+            np.min(measurements, axis=axis),
+            quantile_set,
+        ),
+        'summary_of_medians': summary_arr(
+            np.median(measurements, axis=axis),
+            quantile_set,
+        ),
+    }
+
+
+def save_raw_measurements(output_dir, measurements, measure_id):
+    """Saves raw measures as csv or hdf5 file."""
+    if len(measurements.shape) <= 2:
+        np.savetxt(
+            os.path.join(output_dir, f'{measure_id}.csv'),
+            measurements,
+            delimiter=',',
+        )
+    else:
+        pass
+        # TODO save h5py files
+
+
 def save_measures(
     output_dir,
     measure_id,
@@ -33,15 +73,7 @@ def save_measures(
 ):
     """Convenience function to save measurement output and summarize."""
     if save_raw:
-        if len(measurements.shape) <= 2:
-            np.savetxt(
-                os.path.join(output_dir, f'{measure_id}.csv'),
-                measurements,
-                delimiter=',',
-            )
-        else:
-            pass
-            # TODO save h5py files
+        save_raw_measurements(output_dir, measurements, measure_id)
 
     if quantiles_frac > 2:
         quantile_set = np.arange(1 + quantiles_frac) / quantiles_frac
@@ -51,25 +83,7 @@ def save_measures(
     # Save the summary of the euclidean distances
     io.save_json(
         os.path.join(output_dir, f'{measure_id}_summary.json'),
-        {
-            'overview': summary_arr(measurements, quantile_set=quantile_set),
-            'summary_of_means': summary_arr(
-                np.mean(measurements, axis=1),
-                quantile_set,
-            ),
-            'summary_of_maxs': summary_arr(
-                np.max(measurements, axis=1),
-                quantile_set,
-            ),
-            'summary_of_mins': summary_arr(
-                np.min(measurements, axis=1),
-                quantile_set,
-            ),
-            'summary_of_medians': summary_arr(
-                np.median(measurements, axis=1),
-                quantile_set,
-            ),
-        },
+        summary_dict(measurements, quantile_set, axis=1),
     )
 
     # Save the flattening of the conditionals via different summarization methods
@@ -77,6 +91,23 @@ def save_measures(
         os.path.join(output_dir, f'{measure_id}_target_samples_flat.json'),
         summary_arr(measurements, quantile_set, axis=1),
     )
+
+
+def get_l2dists(targets, preds, prob_simplex_normalize=False, axis=2):
+    """Obtains euclidean distances from sampling of the predictions."""
+    for target_idx in range(len(targets)):
+        # NOTE assumes shape of [targets, conditionals, classes]
+        preds[target_idx] = targets[target_idx] - preds[target_idx]
+    differences = preds
+
+    if prob_simplex_normalize:
+        # Normalizes by the largest possible distance within the probability
+        # simplex, which is the distance form one vertex to any other vertex
+        # because the probability simplex is regular (ie. 2-simplex is a
+        # equilateral triangle).
+        return np.linalg.norm(differences, axis=axis) / np.sqrt(2)
+
+    return np.linalg.norm(differences, axis=axis)
 
 
 if __name__ == '__main__':
