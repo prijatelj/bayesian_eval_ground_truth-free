923eabe91a9be97366a1711329347a92a443e057
prijatelj <theomnipotentwoof@gmail.com>
2020-04-21 20:24:17 -0400

    updated combine to take multiple files.

--- experiment/research/pred/combine.py
+++ experiment/research/pred/combine.py
@@ -6,41 +6,54 @@ import numpy as np
 from experiment import io
 
 def combine(
-    in_file1,
-    in_file2,
+    files,
     out_file,
     shuffle=False,
     keys=['conditionals', 'givens'],
+    seed=1234,
 ):
     """Converts format from `load_fold_save_pred` to given and conditional of
     a single data split (train XOR test)
     """
-    with open(in_file1, 'r') as f:
-        inf1 = json.load(f)
+    key_contents = {key:[] for key in keys}
+    for in_file in files:
+        with open(in_file, 'r') as f:
+            file_contents = json.load(f)
 
-    with open(in_file2, 'r') as f:
-        inf2 = json.load(f)
+            for key in keys:
+                key_contents[key].append(file_contents[key])
 
+    # Stack each key's contents
     for key in keys:
-        inf1[key] = np.vstack((np.array(inf1[key]), np.array(inf2[key])))
+        key_contents[key] = np.vstack(key_contents[key])
 
-    if shuffle:
-        raise NotImplementedError('Shuffling is not implemented yet.')
+        if shuffle:
+            if isinstance(seed, int):
+                np.random.seed(seed)
+            else:
+                raise TypeError(
+                    f'Expected `seed` to be of type `int` not `{type(seed)}`.'
+                )
+            np.random.shuffle(key_contents[key])
 
-    io.save_json(out_file, inf1)
+        # Delete key from file contents; keeping extra details from last JSON
+        del file_contents[key]
+
+    # Add the other details, if any into the new JSON file.
+    # NOTE, expects that the last file to be opened contains all of the
+    # extra content, thus the same content, as the prior files.
+    if file_contents:
+        key_contents.update(file_contents)
+
+    io.save_json(out_file, key_contents)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Combines given JSONs')
 
     parser.add_argument(
-        'in_file1',
-        help='The JSON file in format of `save_pred.load_fold_save_pred()`.',
-    )
-
-    parser.add_argument(
-        'in_file2',
-        help='The JSON file in format of `save_pred.load_fold_save_pred()`.',
+        'files',
+        help='A list of JSON files in format of `save_pred.load_fold_save_pred()`.',
     )
 
     parser.add_argument(
@@ -60,6 +73,13 @@ def parse_args():
         '--shuffle',
         action='store_true',
         help='Shuffles the data after combining.',
+    )
+
+    parser.add_argument(
+        '--seed',
+        type=int,
+        default=1234,
+        help='Seed used to shuffle the contents together the same way.'
     )
 
     return parser.parse_args()
