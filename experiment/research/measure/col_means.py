2bd65303818aef6a4f3cca4d173ef1be5c3544b3
prijatelj <theomnipotentwoof@gmail.com>
2020-05-16 18:11:02 -0400

    by default, ignores non-finite values. Added arg to allow them.

--- experiment/research/measure/col_means.py
+++ experiment/research/measure/col_means.py
@@ -28,12 +28,23 @@ def parse_args():
         help='axis to apply the mean. Default axis = 0',
     )
 
+    parser.add_argument(
+        '--keep_infinte',
+        action='store_true',
+        help='If given, inf and nans are kept and used in the mean',
+    )
+
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
     matrix = np.loadtxt(args.input_file, delimiter=',')
+
+    # remove nans and infs
+    if not args.keep_infinite:
+        matrix = np.ma.masked_array(matrix, mask=~np.isfinite(matrix))
+
     np.savetxt(
         io.create_filepath(args.output_file),
         matrix.mean(axis=args.axis)
