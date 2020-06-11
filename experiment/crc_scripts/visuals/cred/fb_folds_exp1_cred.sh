8e0f6c66f0e80b85777f0799881c392f4dda1b97
prijatelj <theomnipotentwoof@gmail.com>
2020-06-11 14:38:50 -0400

    added exp 1 fb fold visualization scripts.

---
+++ experiment/crc_scripts/visuals/cred/fb_folds_exp1_cred.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#$ -pe smp 4
+#$ -N fb_folds_e1
+#$ -q debug
+#$ -o $HOME/Public/psych_metric/logs/fb/folds/exp1/logs/
+#$ -e $HOME/Public/psych_metric/logs/fb/folds/exp1/logs/
+#$ -t 1-3
+
+# Exp1 Supplemental Experiment: Data Sensitivity / Bayesian Model Generalization
+
+BASE_PATH="$HOME/Public/psych_metric"
+
+# set up the environment
+module add conda
+conda activate psych_exp_tf-1.15 
+
+# Constant paths for all runs
+BASE_DATA="$HOME/scratch_21/results/sjd/3965_exp_test/fb/fq/folds/all_fold_"
+#SRC_PATH="$HOME/scratch_21/results/sjd/3965_exp_test/fb/fq/all_half_1/exp1"
+
+# Unique path addons per run
+if [ "$SGE_TASK_ID" -eq "1" ]; then
+    SRC_PATH="$BASE_DATA"0/exp1
+elif [ "$SGE_TASK_ID" -eq "2" ]; then
+    SRC_PATH="$BASE_DATA"1/exp1
+elif [ "$SGE_TASK_ID" -eq "3" ]; then
+    SRC_PATH="$BASE_DATA"2/exp1
+else
+    echo "ERROR: Unexpected SGE_TASK_ID: $SGE_TASK_ID"
+    exit 1
+fi
+
+# Script to execute
+python3 "$BASE_PATH/experiment/research/measure/cred_interval.py" \
+    "$SRC_PATH/baselines_samples_1.45e4/iid_uniform_dirs/train/euclid_dists_train.csv" \
+    "$SRC_PATH/baselines_samples_1.45e4/iid_dirs_adam/train/euclid_dists_train.csv" \
+    "$SRC_PATH/baselines_samples_1.45e4/dir-mean_mvn-umvu/train/euclid_dists_train.csv" \
+    "$SRC_PATH/baselines_samples_1.45e4/dir-mean_mvn-umvu-zero/train/euclid_dists_train.csv" \
+    "$SRC_PATH/4u_1e-1sim/euclid/train/euclid_dists.csv" \
+    --test_paths "$SRC_PATH/baselines_samples_1.45e4/iid_uniform_dirs/test/euclid_dists_test.csv" \
+    "$SRC_PATH/baselines_samples_1.45e4/iid_dirs_adam/test/euclid_dists_test.csv" \
+    "$SRC_PATH/baselines_samples_1.45e4/dir-mean_mvn-umvu/test/euclid_dists_test.csv" \
+    "$SRC_PATH/baselines_samples_1.45e4/dir-mean_mvn-umvu-zero/test/euclid_dists_test.csv" \
+    "$SRC_PATH/4u_1e-1sim/euclid/test/euclid_dists.csv" \
+    --output_path "$SRC_PATH/cred_95_right_intervals.json" \
+    --conditional_models 'Random\nGuess' 'Dirichlet' 'NDoD' 'NDoD Zero' 'BNN' \
+    --cred_interval 'right' \
+    --credibility .95
